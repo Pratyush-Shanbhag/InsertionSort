897b28afe9b16897ab9903fdff5f6a368e2181de
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-03-12 20:11:45 -0800

    added readFile()

--- ReadWrite.cpp
+++ ReadWrite.cpp
@@ -5,9 +5,18 @@
 using namespace std;
 
 int* ReadWrite::readFile(string fileName) {
+    ifstream infile(fileName);
+    int arr[10];
+    int count = 0;
+    while(!infile.fail()) {
+        infile >> arr[count++];
+    }
 
+    infile.close();
+
+    return arr;
 }
 
 void writeFile(int arr[10]) {
-    
+
 }
