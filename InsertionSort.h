4f556517be968768b667598e5049412dd8433f8d
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-03-12 19:04:31 -0800

    first commit

---
+++ InsertionSort.h
@@ -0,0 +1,12 @@
+#ifndef INSERTIONSORT_H
+#define INSERTIONSORT_H
+
+class InsertionSort {
+
+    public:
+        int arr[10];
+        void insertionSort();
+        void displayArray();
+};
+
+#endif
