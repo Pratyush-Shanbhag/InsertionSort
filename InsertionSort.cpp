a2437490344e33838755def363bdf353395fc67e
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-03-12 19:36:22 -0800

    Added InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "InsertionSort.h"
+
+using namespace std;
+
+void InsertionSort::insertionSort() {
+    int count;
+    int temp;
+
+    for(int i = 0; i < 10; i++) {
+        count = i - 1;
+        temp = arr[i];
+
+        while(count >= 0 && arr[count] < temp) {
+            arr[count+1] = arr[count];
+            count--;
+        }
+
+        arr[count] = temp;
+    }
+} 
